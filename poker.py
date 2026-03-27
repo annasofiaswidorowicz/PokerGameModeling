0d7727ed8117fef6902894818f0804d99c79d5fe
annasofiaswidorowiczlara <annasofiaswidorowicz@gmail.com>
2026-03-27 12:12:06 +0100

    Iterations, hits, flush, full house

--- poker.py
+++ poker.py
@@ -13,8 +13,46 @@ class PokerHand:
         self._cards = _cards
     def __str__(self):
         return str(self._cards)
+    @property
+    def is_flush(self):
+        first_card_suit = self._cards[0].suit
+        for i in range (1, 5):
+            if first_card_suit != self._cards[i].suit:
+                return False
+            return True
 
-hand = PokerHand()
-print(hand)
+    @property
+    def matches(self):
+        matches = 0
+        for card1 in self._cards:
+            for card2 in self._cards:
+                if card1 == card2:
+                    continue
+                if card1.rank == card2.rank:
+                    matches += 1
+        return matches
 
+    @property
+    def is_fullhouse(self):
+        return self.matches == 8
+
+    @property
+    def is_4kind(self):
+        return self.matches == 12
+iterations = 0
+hits = 0
+while True:
+    hand = PokerHand()
+    iterations += 1
+    if hand.is_4kind:
+        print(hand)
+        print("Done it in", iterations)
+        hits += 1
+    if hits == 100:
+        prob = hits / iterations * 100
+        print(f"Probability of a flush is {prob}%")
+        break
+
+#hand = PokerHand()
+#print(hand)
 
